faa7ab7c4386185cf83f9b24c848e75e94e88edc
Steven Sung <steven10400211@gmail.com>
2020-03-05 14:45:15 +0800

    Add main.cpp (exercise program)

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "mbed.h"
+
+
+DigitalOut redLED(LED_RED);
+
+DigitalOut greenLED(LED_GREEN);
+
+
+int main()
+
+{
+
+   redLED = 1;
+
+   greenLED = 1;
+
+   while (true) {
+
+    for(int i=0; i<6; ++i) { //blink for 10 times
+
+        redLED = !redLED; // toggle led
+
+        wait(0.2f);
+
+    }
+
+    for(int i=0; i<4; ++i) { //blink for 10 times
+
+        greenLED = !greenLED; // toggle led
+
+        wait(0.2f);
+
+    }
+
+   }
+
+}
